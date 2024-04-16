190056f5672cb280124171371767cde49c8fac1e
Caden Buckhalt <halaeb@gmail.com>
2024-04-16 14:57:38 -0700

    wip mock prisma client using singleton and dependency injection

--- server/context.ts
+++ server/context.ts
@@ -1,6 +1,8 @@
 import type { Session } from 'lucia';
 import { auth } from '~/utils/auth';
 import * as context from 'next/headers';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 type CreateContextOptions = {
   headers: Headers;
@@ -24,4 +26,19 @@ export const createTRPCContext = async (opts: { req: Request }) => {
     session,
     headers: opts.req.headers,
   });
+};
+
+// Stuff for Unit Testing
+export type Context = {
+  prisma: PrismaClient;
+};
+
+export type MockContext = {
+  prisma: DeepMockProxy<PrismaClient>;
+};
+
+export const createMockContext = (): MockContext => {
+  return {
+    prisma: mockDeep<PrismaClient>(),
+  };
 };
