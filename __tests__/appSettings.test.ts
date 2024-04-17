951f67de27b91d9c6f6cb0fdd83bacd78c274f48
Caden Buckhalt <halaeb@gmail.com>
2024-04-17 07:52:42 -0700

    setting up bun

--- __tests__/appSettings.test.ts
+++ __tests__/appSettings.test.ts
@@ -1,16 +1,12 @@
 import { MockContext, Context, createMockContext } from '~/server/context';
 import { getAppSettings } from '~/server/routers/appSettings';
-import { prismaMock } from '~/singleton';
 
 // import functions
 
 let mockCtx: MockContext;
 let ctx: Context;
 
-beforeEach(() => {
-  mockCtx = createMockContext();
-  ctx = mockCtx as unknown as Context;
-});
+describe;
 
 test('should create new appSettings', async () => {
   const appSettings = {
--- server/context.ts
+++ server/context.ts
@@ -1,8 +1,6 @@
 import type { Session } from 'lucia';
 import { auth } from '~/utils/auth';
 import * as context from 'next/headers';
-import { PrismaClient } from '@prisma/client';
-import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 type CreateContextOptions = {
   headers: Headers;
@@ -26,19 +24,4 @@ export const createTRPCContext = async (opts: { req: Request }) => {
     session,
     headers: opts.req.headers,
   });
-};
-
-// Stuff for Unit Testing
-export type Context = {
-  prisma: PrismaClient;
-};
-
-export type MockContext = {
-  prisma: DeepMockProxy<PrismaClient>;
-};
-
-export const createMockContext = (): MockContext => {
-  return {
-    prisma: mockDeep<PrismaClient>(),
-  };
 };
